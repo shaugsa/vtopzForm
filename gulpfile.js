e77abd27a99a35aa1def100297207602bf2667a3
Игорь Запольский <56001649+shaugsa@users.noreply.github.com>
2022-08-08 13:14:27 +0300

    add clear, browserSync, watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,58 @@
-const {src, dest} = require('gulp');
+const {src, dest, watch, series, parallel} = require('gulp');
+const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
 
+// Plagins
+const fileinclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
+const size = require('gulp-size');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify')
+
+
+// dev HTNL
 const html = () => {
-    return src('./index.html')
+    return src('src/*.html')
+    .pipe(plumber({
+        errorHandler: notify.onError(error => ({
+            title: 'HTML',
+            message: error.message
+        }))
+    }))
+    .pipe(fileinclude())
+    .pipe(size({ title : "До сжатия"}))
+    .pipe(htmlmin({
+        collapseWhitespace: true,
+    }))
+    .pipe(size({ title : "После сжатия"}))
     .pipe(dest('./public'))
+    .pipe(browserSync.stream())
+}
+
+const server = () => {
+    browserSync.init({
+        server: {
+            baseDir: './public'
+        }
+    })
+}
+
+const clear = () => {
+    return src('./public')
+    .pipe(clean())
+}
+
+// watch for HTML
+const watcher = () => {
+    watch('./src/**/*.*', html )
 }
 
 exports.html = html;
+exports.watcher = watcher;
+exports.clear = clear;
+
+exports.dev = series(
+    clear,
+    html,
+    parallel (watcher, server)
+);
