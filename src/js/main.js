1960b9a66f3d15156686844b222ef5b05b0f9f5c
Игорь Запольский <56001649+shaugsa@users.noreply.github.com>
2022-08-14 13:36:37 +0300

    add vivus

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
-import nationalities from './module/nationality.js';
+import {nationalities} from './module/nationality.js';
+// import animation from './module/animation';
 
 window.onload = function() {
+
+    new Vivus('imgVivus', {
+        file: '../img/man.svg',
+        onReady: function (myVivus) {
+            myVivus.el.setAttribute('height', auto)
+        }
+    })
 
     
     const day = document.getElementById('selectDay')
     const month = document.getElementById('selectMonth')
     const year = document.getElementById('selectYear')
+    const nation = document.getElementById('selectNat')
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     const date = new Date();
 
@@ -41,17 +50,19 @@ window.onload = function() {
 
     function getNationalyti() {
         for (let i = 0; i < nationalities.length; i++) {
-
+            let item = document.createElement('option')
+            item.className = 'item';
+            item.value = nationalities[i]
+            item.innerHTML = nationalities[i]
+            nation.append(item)
         }
     }
-
-    console.log(nationalities)
 
     getDay();
     getMonth(months);
     getYear();
+    getNationalyti(nationalities)
 
-    
-    
+
 }
 
